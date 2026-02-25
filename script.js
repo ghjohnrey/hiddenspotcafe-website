d5c105e2f21d241a00e2a83ea38deae99d6e3932
ghjohnrey <johnreymosca@gmail.com>
2026-02-25 12:24:22 +0800

    added a navigation bar

--- script.js
+++ script.js
@@ -109,5 +109,18 @@ function createSparkles(){
 
 createSparkles();
 
+/* ===============================
+   NAVBAR SCROLL SHADOW EFFECT
+=============================== */
+
+window.addEventListener("scroll", function(){
+  const navbar = document.querySelector(".navbar");
+  if(window.scrollY > 30){
+    navbar.classList.add("scrolled");
+  }else{
+    navbar.classList.remove("scrolled");
+  }
+});
+
 
 
