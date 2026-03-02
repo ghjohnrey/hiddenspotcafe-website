91792d98b482c916d76d3f0ed86d9016eb760778
ghjohnrey <johnreymosca@gmail.com>
2026-03-02 21:11:18 +0800

    Update WASM and TTF module declarations

--- og-worker/src/wasm.d.ts
+++ og-worker/src/wasm.d.ts
@@ -1,11 +1,9 @@
-// Type declarations for non-TypeScript assets used in the Worker
-
 declare module "*.wasm" {
-  const mod: WebAssembly.Module;
-  export default mod;
+  const data: ArrayBuffer;
+  export default data;
 }
 
 declare module "*.ttf" {
-  const value: ArrayBuffer;
-  export default value;
+  const data: ArrayBuffer;
+  export default data;
 }
