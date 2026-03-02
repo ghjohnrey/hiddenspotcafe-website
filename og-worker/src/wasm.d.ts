818fc54aee58a67bada7e931af4a6d1091ffa4ef
ghjohnrey <johnreymosca@gmail.com>
2026-03-02 18:02:41 +0800

    Add type declarations for .ttf files

--- og-worker/src/wasm.d.ts
+++ og-worker/src/wasm.d.ts
@@ -1,4 +1,11 @@
+// Type declarations for non-TypeScript assets used in the Worker
+
 declare module "*.wasm" {
   const mod: WebAssembly.Module;
   export default mod;
+}
+
+declare module "*.ttf" {
+  const value: ArrayBuffer;
+  export default value;
 }
